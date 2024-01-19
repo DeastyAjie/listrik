97a6ceddbc2318500902dff207bc6c0b93109148
DeastyAjie <deasty_ajie_32rpl@student.smktelkom-mlg.sch.id>
2024-01-19 09:46:52 +0700

    fist commit

---
+++ LatUKL_SeleksiKondisi1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latukl_seleksikondisi1;
+
+/**
+ *
+ * @author Deasty Ajie
+ */
+public class LatUKL_SeleksiKondisi1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
